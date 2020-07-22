ee06c883286e74ddecf71915016eb3f778adfee9
Luka Tassano <lukatassano@pop-os.localdomain>
2020-07-21 23:48:10 -0300

    models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
 class Category {
+  @PrimaryColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
